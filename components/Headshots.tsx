bd6a855922e1c47c9353093988215eefc5ac779f
Valerian001 <chinecheremvalarian@gmail.com>
2025-09-16 16:39:53 +0100

    .

--- components/Headshots.tsx
+++ components/Headshots.tsx
@@ -1,33 +1,34 @@
 import React from 'react'
 import Image from 'next/image'
+import Assets from '@/constants/assets.constants'
 
 export default function Headshots() {
     return (
         <div>
             <div className="flex headshots [&>img]:rounded-full [&>img]:-mr-2">
                 <Image
-                    src="/headshot (1).png"
+                    src={Assets.headshot1}
                     alt="checkmark"
                     width={32}
                     height={32}
                     className="w-4 h-4 rounded-full sm:w-8 sm:h-8"
                 />
                 <Image
-                    src="/headshot (3).png"
+                    src={Assets.headshot3}
                     alt="checkmark"
                     width={32}
                     height={32}
                     className="w-4 h-4 sm:w-8 sm:h-8"
                 />
                 <Image
-                    src="/headshot (2).png"
+                    src={Assets.headshot2}
                     alt="checkmark"
                     width={32}
                     height={32}
                     className="w-4 h-4 sm:w-8 sm:h-8"
                 />
                 <Image
-                    src="/headshot (4).png"
+                    src={Assets.headshot4}
                     alt="checkmark"
                     width={32}
                     height={32}
